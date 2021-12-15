91c8624c4413c65bd1da53c6d0406fe991d37ae8
David Vergara <david.vergara82141@gmail.com>
2021-12-14 21:50:21 -0500

    Added SpringJdbcPersonsRepository file

---
+++ src/main/java/com/david/inventory/repository/SpringJdbcPersonsRepository.java
@@ -0,0 +1,75 @@
+package com.david.inventory.repository;
+
+import com.david.inventory.domain.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+
+public class SpringJdbcPersonsRepository implements ProductsRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SpringJdbcPersonsRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Product> rowMapper = (resultSet, rowNum) -> {
+        String productId = resultSet.getString("id_product");
+        String productName = resultSet.getString("name");
+        String productCategory = resultSet.getString("category");
+        String productDescription = resultSet.getString("description");
+        return new Product(
+                productId,
+                productName,
+                productCategory,
+                productDescription
+        );
+    };
+
+    @Override
+    public List<Product> findAll() {
+        String sqlQuery = "select * from products ";
+        return jdbcTemplate.query(sqlQuery, rowMapper);
+    }
+
+    @Override
+    public Product findById(String id) {
+        String sqlQuery = "select * from products where id_product = ?";
+        return jdbcTemplate.queryForObject(sqlQuery, rowMapper, id);
+    }
+
+    @Override
+    public List<Product> findByCategory(String category) {
+        String sqlQuery = "select * from products where category = ?";
+        return jdbcTemplate.query(sqlQuery, rowMapper, category);
+    }
+
+    @Override
+    public void create(Product product) {
+        String sqlQuery = "insert into products(id_product, name, category, description) values(?, ?, ?, ?)";
+        jdbcTemplate.update(sqlQuery, ps -> {
+            ps.setString(1, product.getId());
+            ps.setString(2, product.getName());
+            ps.setString(2, product.getCategory());
+            ps.setString(2, product.getDescription());
+        });
+    }
+
+    @Override
+    public void update(String id, Product product) {
+        String sqlQuery = "update products set name = ?, category = ?, description = ? where id_number = ?";
+        jdbcTemplate.update(sqlQuery, ps -> {
+            ps.setString(1, product.getName());
+            ps.setString(2, product.getCategory());
+            ps.setString(3, product.getDescription());
+            ps.setString(4, id);
+        });
+    }
+
+    @Override
+    public void delete(String id) {
+        String sqlQuery = "delete from products where id_product = ?";
+        jdbcTemplate.update(sqlQuery, id);
+    }
+}
